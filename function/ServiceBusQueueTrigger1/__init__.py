a9951e690ee484840f8e9763903604a5e831a1d0
burak <burakilhan@gmail.com>
2021-09-08 12:39:57 +0300

    Add files via upload

---
+++ function/ServiceBusQueueTrigger1/__init__.py
@@ -0,0 +1,64 @@
+import logging
+import azure.functions as func
+import psycopg2
+import os
+from datetime import datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+def main(msg: func.ServiceBusMessage):
+
+    notification_id = int(msg.get_body().decode('utf-8'))
+    logging.info(
+        'Python ServiceBus queue trigger processed message: %s', notification_id)
+
+    conn = psycopg2.connect(dbname="techconfdb", user="serali@hellocelloserver",
+                            password="udacity@2021", host="hellocelloserver.postgres.database.azure.com")
+    print(type(conn))
+    cur = conn.cursor()
+    print(type(cur))
+
+    try:
+
+        cur.execute(
+            f"SELECT message, subject FROM notification WHERE id = {notification_id};")
+        notification = cur.fetchall()
+        print(notification)
+
+        cur.execute("SELECT first_name, last_name, email FROM attendee;")
+        attendees = cur.fetchall()
+
+        for attendee in attendees:
+            subject = f'{attendee[0]}: {notification[0]}'
+
+            print(subject)
+
+            message = Mail(
+                from_email="info@techconf.com",
+                to_emails=f"{attendee[2]}",
+                subject=subject,
+                plain_text_content=f"{notification[0]}")
+
+            print(message)
+
+            sg = SendGridAPIClient(
+                "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")
+            print(sg)
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+
+        notification_completed_date = datetime.utcnow()
+        notification_status = f'Notified {len(attendees)} attendees'
+        cur.execute(
+            f"UPDATE notification SET status = '{notification_status}', completed_date = '{notification_completed_date}' WHERE id = '{notification_id}'")
+
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+    finally:
+        cur.close()
+        conn.close()
---
+++ web/config.py
@@ -0,0 +1,28 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL = "hellocelloserver.postgres.database.azure.com"  # TODO: Update value
+    POSTGRES_USER = "serali@hellocelloserver"  # TODO: Update value
+    POSTGRES_PW = "udacity@2021"  # TODO: Update value
+    POSTGRES_DB = "techconfdb"  # TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(
+        user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING = 'Endpoint=sb://hellocellobus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=0IUcGqV4+WAEYhuLaaZHAlFgCt/o+YQd4l1N1Tc+aFc='  # TODO: Update value
+    SERVICE_BUS_QUEUE_NAME = 'notificationqueue'
+    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
+    SENDGRID_API_KEY = ''  # Configuration not required, required SendGrid Account
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
